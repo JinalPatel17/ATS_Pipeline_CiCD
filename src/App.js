73d74a9537775be52f31dbaf7afa320619e75086
Jinal <pateljinu54625@gmail.com>
2024-03-15 22:24:00 -0600

    package update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ class App extends React.Component {
   componentDidMount() {
     // Fetch invoices from your API here. This is just dummy data.
     const invoices = [
-      { id: 1, customer: 'Customer A', amount: 100 },
-      { id: 2, customer: 'Customer B', amount: 200 },
+   //   { id: 1, customer: 'Customer A', amount: 100 },
+     // { id: 2, customer: 'Customer B', amount: 200 },
     ];
+//<h1>Invoice List</h1>
 
     this.setState({ invoices });
   }
@@ -17,8 +18,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Invoice List</h1>
+        
         <InvoiceList invoices={this.state.invoices} />
+        <h2>Hello Welcome to AutomaticInvoiceSystem</h2>
       </div>
     );
   }
